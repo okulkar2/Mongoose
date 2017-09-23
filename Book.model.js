3c0e0c509f28a5e751a2a9d08bc2482035ad8110
okulkar2 <omkr22@gmail.com>
2017-09-22 22:06:48 -0700

    Book Schema created

--- Book.model.js
+++ Book.model.js
@@ -5,11 +5,8 @@ var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
     title: String,
-    published: {
-        type: Date,
-        default: Date.now
-    },
-    keyword: Array
+    author: String,
+    category: String,
 });
 
 module.exports = mongoose.model('Book', BookSchema);
