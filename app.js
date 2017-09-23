b1420a471f0c640d96d79b8e499cd899207d9094
okulkar2 <omkr22@gmail.com>
2017-09-22 22:04:58 -0700

    Connection to MongoDB complete

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+var Book = require('./Book.model');
+
+var db = 'mongodb://localhost/bookDb'
+var port = process.env.PORT | 300;
+
+mongoose.connect(db);
+
+app.listen(port, function(){
+	console.log('App listening on Port: ' + port);
+}) 
